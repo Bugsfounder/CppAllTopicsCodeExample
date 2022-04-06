ed734977f6c5760afa2fb4bb19eaadd8f17102e2
Bugs Founder <bugsfounder2021@gmail.com>
2022-04-06 05:56:56 +0530

    For, While and do-while loops in C++

---
+++ cppTutorial/10_For,WhileAnddo-whileLoops.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* 
+        Loops in cpp:
+          There are three types if loops in cpp
+          1. for loop
+          2. while loop
+          3. do-while loop
+    */
+    // For Loop
+    // for (size_t i = 0; i < 100; i++)
+    // {
+    //     cout<<i+1<<endl;
+    // }
+    
+    // While loop
+    // int i = 0;
+    // while (i<100)
+    // {
+    //     cout<<i+1<<endl;
+    //     i++;
+    // }
+    
+    // do-while loop 
+    int i = 0;
+    do
+    {
+        cout<<i+1<<endl;
+        i++;
+    } while (i<100);
+    
+
+    return 0;
+}
