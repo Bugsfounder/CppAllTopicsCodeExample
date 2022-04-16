c7bf3066ce74dc4dcb7a00d95c2de78e73e5a424
Bugs Founder <bugsfounder2021@gmail.com>
2022-04-16 13:46:43 +0530

    Cmake and Gtest

---
+++ CMakeTutorial/LibrariesAndSubdirectories/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// float add(float a, float b);
+
+int main(int argc, char const *argv[])
+{
+    cout << "Manisha is a good girl" << endl;
+
+    float s = add(23, 65);
+    cout << "The sum is: " << s << endl;
+
+    return 0;
+}
